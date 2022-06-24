a9c4704abe085bff320aad874b1414a4ea2e3159
dark-king-rayleigh <ayushbaral17@gmail.com>
2022-06-24 12:16:49 +0545

    feat(interface):define interface for company details

---
+++ typing.d.ts
@@ -0,0 +1,21 @@
+export interface CompanyDetails {
+  companyManager: CompanyMaster;
+  companyAddress: Address[];
+}
+
+interface CompanyMaster {
+  companyName: string;
+  website: string;
+  phone: string;
+  email: string;
+}
+
+export interface Address {
+  officeBranch: string;
+  country: string;
+  address1: string;
+  address2: string;
+  addressCode: string;
+  city: string;
+  state: string;
+}
